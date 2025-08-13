5b973dba85e1c915ac8a590f651a23210e216816
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2025-08-13 12:09:15 +0300

    Scalar point addition implemented

--- data_reorganization.cc
+++ data_reorganization.cc
@@ -23,9 +23,12 @@
 
 /* EXAMPLE */
 #include <array>
+#include <chrono>
 #include <immintrin.h>
+#include <iostream>
 
-const int ARR_SIZE = 1000;
+// DEFINING ARR_SIZE
+const int ARR_SIZE = 100000;
 
 struct Point {
   int32_t x, y, z;
@@ -33,12 +36,46 @@ struct Point {
   Point(int x, int y, int z) : x(x), y(y), z(z) {};
 };
 
+// FUNCTION DEFINITIONS
+std::chrono::microseconds scalarAdd(std::array<Point, ARR_SIZE> &points);
+std::chrono::microseconds vectorAdd(std::array<Point, ARR_SIZE> &points);
+
 int main(int argc, char *argv[]) {
-  // Create point array and initiaze the points
+  // CREATING POINT ARRAY AND INITILIAZE THE POINTS
   std::array<Point, ARR_SIZE> points;
+
   for (int i = 0; i < ARR_SIZE; ++i) {
     Point point(i + argc, i + argc, i + argc);
     points[i] = point;
   }
+
+  auto scalarDuration = scalarAdd(points);
+
+  std::cout << "Scalar add took " << scalarDuration.count() << " microseconds."
+            << std::endl;
+
   return 0;
+}
+
+std::chrono::microseconds scalarAdd(std::array<Point, ARR_SIZE> &points) {
+
+  std::array<Point, ARR_SIZE> results;
+  Point barrier;
+
+  // ADDITION OF POINTS
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < ARR_SIZE; ++i) {
+    auto first = points[i];
+    auto second = points[(i + ARR_SIZE / 2) % ARR_SIZE];
+    results[i] =
+        Point(first.x + second.x, first.y + second.y, first.z + second.z);
+    barrier.x = results[i].x;
+    barrier.y = results[i].y;
+    barrier.z = results[i].z;
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  //
+  asm volatile("" ::"g"(barrier) : "memory");
+
+  return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 }
